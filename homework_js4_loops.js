d9bc10a4889df354d5550a6f0e5afde2d6a0814a
Alina Konoplenko <aflorinskaa@gmail.com>
2023-08-31 13:50:42 +0200

    added_homework4_file

---
+++ homework_js4_loops.js
@@ -0,0 +1,16 @@
+// rewrite to while
+// for (let i = 0; i < 3; i++) {
+//   alert( `number ${i}!` );
+// }
+
+
+
+// create a loop from 0 to 10, inside create a variable 'NotDivide', and assign numbers that can't divide 2 and console every time that number. 
+
+
+
+// create loop with condition from 0 to 10, and jump to the next iteration when i is 5.
+
+
+
+// create if/else for checking numbers, if lower than 10 console 'Hello', if higher than 10 console 'Bye'
