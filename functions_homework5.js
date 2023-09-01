5fbd77de2b881f6f229ae79d8f691af7e4cc4b22
Alina Konoplenko <aflorinskaa@gmail.com>
2023-09-01 17:13:31 +0200

    added_functions_homework5

---
+++ functions_homework5.js
@@ -0,0 +1,17 @@
+// create a function that takes one param and inside makes the value Uppercase
+// 	if the parameter not string type - console message about the incorrect type of data.
+
+
+//  create js function that add properties for the object, allowing only number
+
+
+// create JS object with 5 different properties
+
+
+// with previously created function add a new property
+
+
+// check the function syntax and if some issues propose
+// let sum == function{a, b} (
+//   return a + b;
+// )
