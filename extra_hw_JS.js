ce5972566435e7d65ee63d1efbaeda76bc48c925
Alina Konoplenko <aflorinskaa@gmail.com>
2023-09-04 21:50:24 +0200

    added_extra_hw

--- extra_hw_JS.js
+++ extra_hw_JS.js
@@ -1,3 +1,20 @@
-const extra = require('./extra_hw.json')
+const fs = require('fs-extra')
+const { parse } = require('path')
 
-console.log(extra)
+const path = './extra_hw.json'
+
+fs.readFile(path, (err, obj) => {
+    if (err) console.error("bloody hell")
+    let parsedJson = JSON.parse(obj)
+    console.log(parsedJson);
+
+    parsedJson["age"] = 22
+    let jsonAsString = JSON.stringify(parsedJson)
+    console.log(jsonAsString)
+    
+    fs.writeFile(path, jsonAsString, err => {
+        if (err) return console.error(err)
+        console.log('Update was successful!')
+    })
+
+})
