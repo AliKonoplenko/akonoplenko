123cfd11e69f17a3b4bfc882c0551e25338707c6
Alina Konoplenko <aflorinskaa@gmail.com>
2023-09-04 21:57:04 +0200

    Revert "added_extra_hw"
    This reverts commit ce5972566435e7d65ee63d1efbaeda76bc48c925.

--- extra_hw_JS.js
+++ extra_hw_JS.js
@@ -1,20 +1,3 @@
-const fs = require('fs-extra')
-const { parse } = require('path')
+const extra = require('./extra_hw.json')
 
-const path = './extra_hw.json'
-
-fs.readFile(path, (err, obj) => {
-    if (err) console.error("bloody hell")
-    let parsedJson = JSON.parse(obj)
-    console.log(parsedJson);
-
-    parsedJson["age"] = 22
-    let jsonAsString = JSON.stringify(parsedJson)
-    console.log(jsonAsString)
-    
-    fs.writeFile(path, jsonAsString, err => {
-        if (err) return console.error(err)
-        console.log('Update was successful!')
-    })
-
-})
+console.log(extra)
