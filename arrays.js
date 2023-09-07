0d0615ab666c7cd9a1cda4ac76feed1318480be8
Alina Konoplenko <aflorinskaa@gmail.com>
2023-09-07 18:40:33 +0200

    add

---
+++ arrays.js
@@ -0,0 +1,14 @@
+let mArr22 = [
+    [1, 3, 'Shrek'],
+    [4, 10],
+    [40, 50]
+]
+// let nameShrek = mArr22[0][2]
+// console.log(nameShrek)
+
+ for (let i = 0; i < mArr22.length; i++) {
+    let smallArr = mArr22[i]
+    for (let y = 0; y < smallArr.length; y++){
+        console.log(smallArr[y])
+ }
+ }
