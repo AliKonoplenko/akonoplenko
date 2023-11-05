41e2a3e4805f818a882901cd4754db7b3bbeae32
Alina Konoplenko <aflorinskaa@gmail.com>
2023-11-05 13:09:40 +0100

    added_base_of_usage_mongo

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from "cypress";
 import fs from "fs-extra";
+import { configurePlugin } from "cypress-mongodb";
 
 export default defineConfig({
+  env: {
+    mongodb: {
+      uri: 'mongodb://127.0.0.1:27017',
+      database: 'test'
+    }
+  },
   e2e: {
     experimentalStudio: true,
     viewportHeight: 1080,
@@ -13,6 +20,7 @@ export default defineConfig({
       runMode: 2
     },
     setupNodeEvents(on, config) {
+      on('task', {log(message) {console.log(message); return null }})
       on('task', {
         saveURL(url) {
           fs.writeFile('./hw11.json', url , err => {
@@ -21,7 +29,8 @@ export default defineConfig({
           })
           return null
         }
-      })
+      });
+      configurePlugin(on)
     }
   }
 });
