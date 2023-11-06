2f67bbc984ed7ab2a10b705bad9431641e50815f
Alina Konoplenko <aflorinskaa@gmail.com>
2023-11-06 14:24:37 +0100

    added_hw14

--- cypress/e2e/custom_scenarios/hw14_faker.cy.js
+++ cypress/e2e/custom_scenarios/hw14_faker.cy.js
@@ -1,26 +1,39 @@
-///  <reference types="cypress" />
+import { ObjectId } from 'mongodb'
 import { faker } from '@faker-js/faker';
 
 
-describe('login with api request', () => {
+describe('HW_14', () => {
 
-    beforeEach(() => {
-        cy.loginAndSetLocalStorage()
-    })
+    context('Faker_using', () => {
 
-    let category
+        it('create user', () => {
+            cy.request({
+                method: 'POST',
+                url: 'http://localhost:3000/api/users/register',
+                headers: {
+                    Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NTQ3N2ZhMWVjYjI5ZTBmY2VhYTI5MzciLCJpYXQiOjE2OTkxODYzMTQsImV4cCI6MTY5OTc5MTExNH0.OCNDBy8a7xFXDYWdvMuyq5Pi3ANo-JY8noFBybGRWlo'
+                },
+                body: {
+                    firstName: faker.person.firstName(),
+                    lastName: faker.person.lastName(),
+                    username: faker.internet.userName(),
+                    password: faker.internet.password()
+                }
+            })
 
-    it('create category', () => {
-        cy.createNewCategory('TEST CATEGORY', (response) => {
-            category = response
         })
-      
+
+        it('username checking', () => {
+            cy.request({
+                method: 'GET',
+                url: 'http://localhost:3000/api/users',
+                headers: {
+                    Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NTQ3N2ZhMWVjYjI5ZTBmY2VhYTI5MzciLCJpYXQiOjE2OTkxODYzMTQsImV4cCI6MTY5OTc5MTExNH0.OCNDBy8a7xFXDYWdvMuyq5Pi3ANo-JY8noFBybGRWlo'
+                }
+            }).then(({ body }) => {
+                expect(JSON.stringify(body)).to.include('username')
+            })
+        })
     })
 
-    it('create position', () => {
-        cy.createNewPosition('cypress', '3000', category)
-        cy.visit('http://5.189.186.217/categories/' + category)
-    })
 })
-
-
