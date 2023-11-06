600035455e8badd6cc9c3e1a393f9470d641eb90
Alina Konoplenko <aflorinskaa@gmail.com>
2023-11-06 11:30:29 +0100

    added_last_changes

--- cypress/e2e/custom_scenarios/query_mongo.cy.js
+++ cypress/e2e/custom_scenarios/query_mongo.cy.js
@@ -3,7 +3,7 @@ import { ObjectId } from 'mongodb'
 
 describe('Find data', () => {
 
-    conteXt('usage of mongodb', () => {
+    context('usage of mongodb', () => {
         it('findMany', () => {
             cy.findMany({}, { collection: 'users' }).then((mongoresult) => {
                 cy.task('log', mongoresult)
@@ -12,5 +12,25 @@ describe('Find data', () => {
 
     })
 
+    context('UI & API request', () => {
+        it('create user', () => {
+            cy.request({
+                method: 'POST',
+                url: 'http://localhost:3000/api/users/register',
+                headers: {
+                    Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NTQ3N2ZhMWVjYjI5ZTBmY2VhYTI5MzciLCJpYXQiOjE2OTkxODYzMTQsImV4cCI6MTY5OTc5MTExNH0.OCNDBy8a7xFXDYWdvMuyq5Pi3ANo-JY8noFBybGRWlo'
+                },
+                body: {
+                    firstName: 'sss',
+                    lastName: 'qqq',
+                    username: 'Mi',
+                    password: '123qweasd'
+                }
+            })
+
+        })
+
+    })
 
 })
+
